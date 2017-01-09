145a1b077f2c28a2c6ada2a49d5048e2b372a70d
tasnimh22 <tasnimh4@gmail.com>
2017-01-09 22:25:22 +0000

    i changed stuff

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Write your function calls below this line
+setColor(2,  0, "purple"); 
+setColor(2,  1, "pink");
+setColor(0,  9, "orange");
+setColor(0,  2, "red");
+setColor(0,  1,"pink");
+setColor(0,  0,"purple");
+setColor(0,  3,"yellow");
+setColor(0,  4,"blue")
+setColor(0,  5,"green");
